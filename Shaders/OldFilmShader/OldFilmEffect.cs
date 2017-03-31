561387b46fc874ef7f46ad1c43d226c633879b07
stzb321 <445471528@qq.com>
2017-03-31 20:48:03 +0800

    旧电影风格的shader

---
+++ Shaders/OldFilmShader/OldFilmEffect.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class OldFilmEffect : MonoBehaviour
+{
+
+    public Shader Shader = null;
+    //[Range(0, 1)]
+    public Color addtiveColor;
+
+    [Range(0, 1)]
+    public float HoloAmount = 1.0f;
+    public Texture HoloTex;
+
+    [Range(0, 1)]
+    public float DustAmount = 1.0f;
+    public Texture DustTex;
+
+    [Range(0, 1)]
+    public float LinesAmount = 1.0f;
+    public Texture LinesTex;
+    [Range(0, 1)]
+    public float LinesXSpeed = 1.0f;
+    [Range(0, 1)]
+    public float LinesYSpeed = 1.0f;
+
+    [Range(0, 1)]
+    public float EffectAmount = 1.0f;
+
+    private float RandomVal;
+
+    static Material m_Material = null;
+    protected Material material
+    {
+        get
+        {
+            if (m_Material == null)
+            {
+                m_Material = new Material(Shader);
+                m_Material.hideFlags = HideFlags.DontSave;
+            }
+            return m_Material;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        if (!SystemInfo.supportsImageEffects)
+        {
+            enabled = false;
+            return;
+        }
+        // Disable if the shader can't run on the users graphics card
+        if (!Shader || !material.shader.isSupported)
+        {
+            enabled = false;
+            return;
+        }
+	}
+
+    public void Update()
+    {
+        RandomVal = Random.Range(-1f, 1f);
+    }
+
+	
+	// Called by the camera to apply the image effect
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Debug.Log(string.Format("width = {0}, height = {1}", source.width , source.height));
+        //RenderTexture buffer = RenderTexture.GetTemporary(source.width, source.height, 0);
+        material.SetColor("_AddTiveColor", addtiveColor);
+        material.SetFloat("_HoloAmount", HoloAmount);
+        material.SetTexture("_HoloTex", HoloTex);
+        material.SetFloat("_DustAmount", DustAmount);
+        material.SetTexture("_DustTex", DustTex);
+        material.SetFloat("_LinesAmount", LinesAmount);
+        material.SetTexture("_LinesTex", LinesTex);
+        material.SetFloat("_LinesXSpeed", LinesXSpeed);
+        material.SetFloat("_LinesYSpeed", LinesYSpeed);
+        material.SetFloat("_RandomVal", RandomVal);
+        material.SetFloat("_EffectAmount", EffectAmount);
+        Graphics.Blit(source, destination, material);
+    }
+}
---
+++ Shaders/OldFilmShader/OldFilmEffect.shader
@@ -0,0 +1,86 @@
+﻿Shader "Hidden/OldFilmEffect"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_HoloTex ("HoloTex", 2D) = "white" {}
+		_HoloAmount("HoloAmount", Range(0.0, 1.0)) = 1.0
+		_DustTex ("DustTex", 2D) = "white" {}
+		_DustAmount("DustAmount", Range(0.0, 1.0)) = 1.0
+		_LinesTex ("LinesTex", 2D) = "white" {}
+		_LinesAmount("LinesAmount", Range(0.0, 1.0)) = 1.0
+		_LinesXSpeed("LinesXSpeed", Range(0.0, 1.0)) = 1.0
+		_LinesYSpeed("LinesYSpeed", Range(0.0, 1.0)) = 1.0
+		_RandomVal("RandomVal", Range(-1.0, 1.0)) = 1.0
+		_EffectAmount("EffectAmount", Range(0.0, 1.0)) = 1.0
+		_AddTiveColor("AddTiveColor", Color) = (1,1,1,1)
+	}
+	SubShader
+	{
+		// No culling or depth
+		Cull Off ZWrite Off ZTest Always
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+			
+			sampler2D _MainTex;
+			sampler2D _HoloTex;
+			float _HoloAmount;
+			sampler2D _DustTex;
+			float _DustAmount;
+			sampler2D _LinesTex;
+			float _LinesAmount;
+			float _LinesXSpeed;
+			float _LinesYSpeed;
+			float _RandomVal;
+			fixed4 _AddTiveColor;
+			float _EffectAmount;
+
+			fixed4 frag (v2f i) : SV_Target
+			{
+				half2 renderTexUV = half2(i.uv.x, i.uv.y + (_RandomVal * _SinTime.z * 0.005));
+				fixed4 col = tex2D(_MainTex, renderTexUV);
+				fixed lum = dot(fixed3(0.299, 0.587, 0.114), col.rgb);
+				fixed4 final = lum + lerp(_AddTiveColor, _AddTiveColor + fixed4(0.1f, 0.1f, 0.1f, 0.1f), _RandomVal);
+
+				half2 LineUV = half2(i.uv.x + (_RandomVal * _Time.x * _LinesXSpeed), i.uv.y + (_Time.x * _LinesYSpeed));
+
+				fixed4 HoloCol = tex2D(_HoloTex, i.uv);
+				fixed4 DustCol = tex2D(_DustTex, i.uv);
+				fixed4 LinesCol = tex2D(_LinesTex, LineUV);
+				final = lerp(final, final * HoloCol, _HoloAmount);
+				final = lerp(final, final * DustCol, _DustAmount);
+				final = lerp(final, final * LinesCol, _LinesAmount);
+				final = lerp(col, final, _EffectAmount);
+
+				return final;
+			}
+			ENDCG
+		}
+	}
+}
